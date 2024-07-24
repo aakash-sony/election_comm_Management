754496d4fceabca64da39cbdbb8d81294c7fbb8c
Akash <aakashsoni8756@gmail.com>
2024-07-25 01:16:14 +0530

    Election commission demo Project

--- model/MainClass.java
+++ model/MainClass.java
@@ -7,18 +7,18 @@ public class MainClass
 
 	public static void main(String[] args) throws IOException
 	{
-//		//Insert political parties
-//		Factory.insertPoliticalParties();
-//		Factory.getPartyDetail();
-//		
-//		//Insert constituency details
-//		
-//		Factory.insertConstituencyDetails();
-//		Factory.getConstituencyDetails();
-//		
-//		//Insert candidate details
-//		Factory.insertCandidateDetail();
-//		Factory.getCandidateDetails();
+		//Insert political parties
+		Factory.insertPoliticalParties();
+		Factory.getPartyDetail();
+		
+		//Insert constituency details
+		
+		Factory.insertConstituencyDetails();
+		Factory.getConstituencyDetails();
+		
+		//Insert candidate details
+		Factory.insertCandidateDetail();
+		Factory.getCandidateDetails();
 		String str = "java is programing language";
 		System.out.println(str.split(" ").length);
 	}
